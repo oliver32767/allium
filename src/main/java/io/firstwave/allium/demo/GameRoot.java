f9751da683f3f1913931d3601e1b6654e09bf631
Oliver B <olbartley@gmail.com>
2015-12-08 03:28:21 -0800

    Add sequence layer

--- src/main/java/io/firstwave/allium/demo/GameRoot.java
+++ src/main/java/io/firstwave/allium/demo/GameRoot.java
@@ -6,7 +6,10 @@ import io.firstwave.allium.api.inject.Inject;
 import io.firstwave.allium.api.layer.AnnotationLayer;
 import io.firstwave.allium.api.layer.GridLayer;
 import io.firstwave.allium.api.layer.NoiseLayer;
+import io.firstwave.allium.api.options.DoubleOption;
+import io.firstwave.allium.api.options.Options;
 import io.firstwave.allium.demo.riemann.RiemannLayer;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 /**
@@ -23,7 +26,19 @@ public class GameRoot extends Layer {
     @Inject
     AnnotationLayer anno;
 
+    @Inject
+    Layer sequence;
+
+    @Inject
+    private double sequenceDecay;
+
+    private static final int T_MINUS = 5;
+
     public GameRoot() {
+        super(null, Options.create()
+                .add("sequenceDecay", new DoubleOption(0.95, 0, 1))
+                .build()
+        );
         Layer gl = new GridLayer();
         gl.setVisible(false);
         addChild(gl);
@@ -33,6 +48,7 @@ public class GameRoot extends Layer {
         addChild(new NoiseLayer("noise"));
         addChild(new RiemannLayer("riemann"));
         addChild(new AnnotationLayer("anno"));
+        addChild(new Layer("sequence"));
     }
 
     @Override
@@ -41,8 +57,38 @@ public class GameRoot extends Layer {
         noise.render(ctx);
         riemann.render(ctx);
 
+        double x = -1;
+        double y = -1;
+
+        GraphicsContext gc = sequence.getCanvas().getGraphicsContext2D();
+        Color c = Color.PINK;
+
+        double alpha;
+        final double decay = sequenceDecay;
+
         for (int i = 0; i < riemann.getShapes().size() && i < 100; i++) {
-            anno.addAnnotation(new AnnotationLayer.Annotation(String.valueOf(i), riemann.getShapes().get(i).x, riemann.getShapes().get(i).y));
+            final double newX = riemann.getShapes().get(i).x;
+            final double newY = riemann.getShapes().get(i).y;
+
+            if (x >= 0) {
+                gc.setStroke(c);
+                gc.strokeLine(x, y, newX, newY);
+            }
+
+            alpha = c.getOpacity() * decay;
+            if (alpha < 0) {
+                break;
+            }
+
+            c = new Color(c.getRed(), c.getGreen(), c.getBlue(), alpha);
+
+            x = newX;
+            y = newY;
+
+
+            anno.addAnnotation(new AnnotationLayer.Annotation(String.valueOf(i - T_MINUS),
+                    riemann.getShapes().get(i).x,
+                    riemann.getShapes().get(i).y));
         }
     }
 }
